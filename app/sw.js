ba42a532764f1003e31d3578f87bef40953b3460
Marcus Chiu <marcuschiu9@gmail.com>
2019-12-20 12:24:48 -0600

    finished codelab

--- app/sw.js
+++ app/sw.js
@@ -1,22 +1,51 @@
-/*
-*
-*  Push Notifications codelab
-*  Copyright 2015 Google Inc. All rights reserved.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License
-*
-*/
-
-/* eslint-env browser, serviceworker, es6 */
-
 'use strict';
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'TITLE';
+  const options = {
+    title: 'options title',
+    body: `${event.data.text()}`,
+    icon: 'images/icon.png',
+    badge: 'images/badge.png',
+//    vibrate: [300,100,400,100,400,100,400],
+    data: {
+      url_1:'http://www.marcuschiu.com',
+      url_2: 'http://confluence.marcuschiu.com'
+    },
+    actions: [
+      {action: "open_url_1", title: "options action 1"},
+      {action: "open_url_2", title: "options action 2"}
+    ]
+  };
+
+  const notificationPromise = self.registration.showNotification(title, options);
+
+  // event.waitUntil(). This method takes a promise to enable the browser to keep
+  // your service worker alive and running until the promise passed in has been resolved
+  event.waitUntil(notificationPromise);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click received.');
+
+  // close the notification that was clicked
+  event.notification.close();
+
+  let promiseOpenWindow;
+  switch(event.action){
+      case 'open_url_1':
+        promiseOpenWindow = clients.openWindow(event.notification.data.url_1); // which we got from above
+      break;
+      case 'open_url_2':
+        promiseOpenWindow = clients.openWindow(event.notification.data.url_2); // which we got from above
+      break;
+      case 'any_other_action':
+        promiseOpenWindow = clients.openWindow("https://www.example.com");
+      break;
+  }
+
+  event.waitUntil(promiseOpenWindow);
+});
